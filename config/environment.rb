65bc01b3c959db4688a726132454313de69f09c4
Péter Schmíz <peter.schmiz@carnationgroup.com>
2014-10-26 20:35:59 +0100

    Heroku fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+	:address        => 'smtp.googlemail.com',
+	:port           => 587,
+	:authentication => :plain,
+	:user_name      => 'pety7439@gmail.com',
+	:password       => 'kretenps7439',
+	:domain         => 'herokuapp.com',
+	:enable_starttls_auto => true
+}
