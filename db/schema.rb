2b655dd28da70cb9640bf1e91fa82fbae82bc080
Péter Schmíz <peter.schmiz@carnationgroup.com>
2014-10-26 20:10:31 +0100

    Add account activation, password resets & email configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026140257) do
+ActiveRecord::Schema.define(version: 20141026183226) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
