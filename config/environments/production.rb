78bacd0767d83ba4306811aa9e6ac3a144e6ba10
Péter Schmíz <peter.schmiz@carnationgroup.com>
2014-10-26 20:27:22 +0100

    Fixed Gmail bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,9 @@
 Rails.application.configure do
 
   config.cache_classes = true
-
   config.eager_load = true
-
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
   config.serve_static_assets = false
 
   config.assets.js_compressor = :uglifier
@@ -14,26 +11,24 @@ Rails.application.configure do
   config.assets.digest = true
 
   config.force_ssl = true
-
   config.i18n.fallbacks = true
-
   config.active_support.deprecation = :notify
-
   config.log_formatter = ::Logger::Formatter.new
-
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   host = 'flow-sample-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
   ActionMailer::Base.smtp_settings = {
 	  :address        => 'smtp.googlemail.com',
 	  :port           => 587,
 	  :authentication => :plain,
 	  :user_name      => 'pety7439@gmail.com',
 	  :password       => 'kretenps7439',
-	  :domain         => 'heroku.com',
+	  :domain         => 'herokuapp.com',
 	  :enable_starttls_auto => true
   }
 end
